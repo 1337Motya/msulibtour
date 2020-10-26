046202cfd4ad4b496011809d6a67ecea04ed0516
Matvey <matvey2k@gmail.com>
2020-10-26 18:09:38 +0300

    Обновлены названия файлов-скриптов сцен

---
+++ src/js/delivery-desk.js
@@ -0,0 +1,196 @@
+pannellum.viewer('train_scene', {
+    "autoLoad": true,   
+    "showControls": false,
+    "compass": false,
+    "hfov": 180,
+    "autoRotate": -2,
+    "default": {
+        "firstScene": "second_floor",
+    },
+
+    "scenes": {
+        "second_floor": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/1.jpg",
+            'yaw': 60,
+            "hotSpots": [
+                {
+                    "pitch": -5,
+                    "yaw": 58,
+                    "type": "scene",
+                    "text": "Абонемент учебной литературы",
+                    "sceneId": "educational_literature"
+                },
+                {
+                    "pitch": -5,
+                    "yaw": 40,
+                    "type": "scene",
+                    "text": "Лестница вниз",
+                    "sceneId": "first_floor_corridor_start"
+                },
+                {
+                    "pitch": 7,
+                    "yaw": 150,
+                    "type": "info",
+                    "text": "Структура библиотеки. Объявления",
+                    "URL": "https://library.msu.by/o-biblioteke/struktura-biblioteki"
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 105,
+                    "type": "info",
+                    "text": "Списки новой литературы",
+                    "URL": "https://library.msu.by/resursy/spisok-novoj-literatury-arkhiv"
+                }
+            ]
+        },
+        "educational_literature": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/2.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -13,
+                    "yaw": 172,
+                    "type": "scene",
+                    "text": "Коридор",
+                    "sceneId": "second_floor"
+                },
+                {
+                    "pitch": -7,
+                    "yaw": 360,
+                    "type": "info",
+                    "text": "Кафедра обслуживания пользователей"
+                },
+                {
+                    "pitch": 6,
+                    "yaw": 310,
+                    "type": "info",
+                    "text": "Фонд учебной, учебно-методической литературы"
+                },
+                {
+                    "pitch": -4,
+                    "yaw": 264,
+                    "type": "info",
+                    "text": "Выставка новых учебных изданий"
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 84,
+                    "type": "info",
+                    "text": "Администрация библиотеки"
+                }
+            ]
+        },
+        "first_floor_corridor_start": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/3.jpg",
+            "hotSpots": [
+                {
+                    "pitch": 2,
+                    "yaw": -11,
+                    "type": "scene",
+                    "text": "Лестница наверх",
+                    "sceneId": "second_floor"
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 1,
+                    "type": "scene",
+                    "text": "Абонемент научной и художественной литературы",
+                    "sceneId": "scientific_literature"
+                },
+                {
+                    "pitch": -8,
+                    "yaw": 180,
+                    "type": "scene",
+                    "text": "Конец коридора",
+                    "sceneId": "first_floor_corridor_end"
+                }
+            ]
+        },
+        "scientific_literature": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/4.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -9,
+                    "yaw": 181,
+                    "type": "scene",
+                    "text": "Коридор",
+                    "sceneId": "first_floor_corridor_start"
+                },
+                {
+                    "pitch": -9,
+                    "yaw": 359,
+                    "type": "info",
+                    "text": "Кафедра обслуживания пользователей"
+                },
+                {
+                    "pitch": 6,
+                    "yaw": 340,
+                    "type": "info",
+                    "text": "Фонд учебной, учебно-методической литературы"
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 108,
+                    "type": "info",
+                    "text": "Администрация библиотеки"
+                }
+            ]
+        },
+        "first_floor_corridor_end": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/5.jpg",
+            "yaw": 130,
+            "hotSpots": [
+                {
+                    "pitch": -9,
+                    "yaw": 146,
+                    "type": "scene",
+                    "text": "Читальный зал редких изданий",
+                    "sceneId": "rare_publications"
+                },
+                {
+                    "pitch": -3,
+                    "yaw": 1,
+                    "type": "scene",
+                    "text": "Начало коридора",
+                    "sceneId": "first_floor_corridor_start"
+                },
+                {
+                    "pitch": -4,
+                    "yaw": -16,
+                    "type": "info",
+                    "text": "Отдел формирования информационных ресурсов"
+                }
+            ]
+        },
+        "rare_publications": {
+            "type": "equirectangular",
+            "panorama": "../img/delivery-desk/6.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -5,
+                    "yaw": 70,
+                    "type": "scene",
+                    "text": "Коридор",
+                    "sceneId": "first_floor_corridor_end"
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 293,
+                    "type": "info",
+                    "text": "Информационная выставка раритетов",
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 280,
+                    "type": "info",
+                    "text": "Фонд редких изданий",
+                    "URL": "https://library.msu.by/resursy/fondy-i-kollektsii/fond-redkikh-izdanij"
+                }
+            ]
+        }
+    }
+});
---
+++ src/js/reading-hall.js
@@ -0,0 +1,188 @@
+pannellum.viewer('rd_scene', {
+    "autoLoad": true,
+    "showControls": false,
+    "compass": false,
+    "hfov": 180,
+    "autoRotate": -2,
+    "default": {
+        "firstScene": "corridor",
+    },
+
+    "scenes": {
+        "corridor": {
+            "type": "equirectangular",
+            "panorama": "../img/reading-hall/1.jpg",
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": 370,
+                    "type": "scene",
+                    "text": "Читальный зал",
+                    "sceneId": "reception"
+                }
+            ]
+        },
+        "reception": {
+            "type": "equirectangular",
+            "panorama": "../img/reading-hall/2.jpg",
+            "yaw": 180,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -7,
+                    "type": "scene",
+                    "text": "Коридор",
+                    "sceneId": "corridor"
+                },
+                {
+                    "pitch": -8,
+                    "yaw": 90,
+                    "type": "info",
+                    "text": "Кафедра выдачи литературы",
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 75,
+                    "type": "info",
+                    "text": "Книжный фонд читального зала",
+                },
+                {
+                    "pitch": 2,
+                    "yaw": 270,
+                    "type": "info",
+                    "text": "Тематические выставки",
+                },
+                {
+                    "pitch": 350,
+                    "yaw": 180,
+                    "type": "scene",
+                    "text": "Зал",
+                    "sceneId": "hall"
+                }
+            ]
+        },
+        "hall": {
+            "type": "equirectangular",
+            "panorama": "../img/reading-hall/3.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -12,
+                    "yaw": 224,
+                    "type": "scene",
+                    "text": "Холл",
+                    "sceneId": "reception"
+                },
+                {
+                    "pitch": -12,
+                    "yaw": 46,
+                    "type": "scene",
+                    "text": "Центр",
+                    "sceneId": "hall_center"
+                },
+                {
+                    "pitch": 4,
+                    "yaw": 248,
+                    "type": "info",
+                    "text": "Фонд периодических изданий",
+                },
+                {
+                    "pitch": -2,
+                    "yaw": 263,
+                    "type": "info",
+                    "text": "Кафедра обслуживания пользователей",
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 128,
+                    "type": "info",
+                    "text": "Выставка литературных новинок",
+                }
+            ]
+        },
+        "hall_center": {
+            "type": "equirectangular",
+            "panorama": "../img/reading-hall/4.jpg",
+            "yaw": -100,
+            "hotSpots": [
+                {
+                    "pitch": -12,
+                    "yaw": 145,
+                    "type": "scene",
+                    "sceneId": "hall"
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 78,
+                    "type": "scene",
+                    "text": "Читальный зал для преподавателей",
+                    "sceneId": "professor_reading_room"
+                },
+                {
+                    "pitch": -2,
+                    "yaw": 220,
+                    "type": "info",
+                    "text": "Доступ к ресурсам сети Интернет",
+                },
+                {
+                    "pitch": -2,
+                    "yaw": 235,
+                    "type": "info",
+                    "text": "Электронный каталог",
+                    "URL": "https://catalog.msu.by"
+                },
+                {
+                    "pitch": -2,
+                    "yaw": 250,
+                    "type": "info",
+                    "text": "Электронный архив библиотеки",
+                    "URL": "https://library.msu.by/resursy/otsifrovannye-izdaniya"
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 294,
+                    "type": "info",
+                    "text": "Проведение мероприятий в онлайн-формате"
+                },
+                {
+                    "pitch": 4,
+                    "yaw": 60,
+                    "type": "info",
+                    "text": "Фотовыставки. Экскурсии. Презентации"
+                }
+            ]
+        },
+        "professor_reading_room": {
+            "type": "equirectangular",
+            "panorama": "../img/reading-hall/5.jpg",
+            "yaw": 70,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": 118,
+                    "type": "scene",
+                    "text": "Читальный зал",
+                    "sceneId": "hall_center"
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 95,
+                    "type": "info",
+                    "text": "Выставка работ преподавателей"
+                },
+                {
+                    "pitch": -6,
+                    "yaw": 54,
+                    "type": "info",
+                    "text": "Доступ к ресурсам сети Интернет"
+                },
+                {
+                    "pitch": 0,
+                    "yaw": 180,
+                    "type": "info",
+                    "text": "Поддержка публикационной активности преподавателей в web-рейтингах",
+                    "URL": "https://www.elibrary.ru/authors.asp"
+                }
+            ]
+        }
+    }
+});
